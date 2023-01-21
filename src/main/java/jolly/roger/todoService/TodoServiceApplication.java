85ee109d6c9a6c8fbd81c0b7602fa8edbd79a009
xurror <kazenasser@gmail.com>
2023-01-21 05:18:06 +0100

    Initialize simple TODO service

---
+++ src/main/java/jolly/roger/todoService/TodoServiceApplication.java
@@ -0,0 +1,13 @@
+package jolly.roger.todoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jolly/roger/todoService/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jolly.roger.todoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
